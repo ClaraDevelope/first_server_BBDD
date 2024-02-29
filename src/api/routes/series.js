8f54d9b1556140a7cffefe0d233265ad0138bad3
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-02-29 19:58:08 +0100

    iniciado

--- src/api/routes/series.js
+++ src/api/routes/series.js
@@ -3,6 +3,7 @@ const {
   getSeriesById,
   getSeriesByGender,
   postSeries,
+  putSeries,
   deleteSeries
 } = require('../controllers/series')
 
@@ -12,6 +13,7 @@ seriesRouter.get('/', getSeries)
 seriesRouter.get('/:id', getSeriesById)
 seriesRouter.get('/gender/:gender', getSeriesByGender)
 seriesRouter.post('/', postSeries)
+seriesRouter.put('/:id', putSeries)
 seriesRouter.delete('/:id', deleteSeries)
 
 module.exports = { seriesRouter }
